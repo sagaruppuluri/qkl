a00636d5960334d0715754bd0d9d89cd221e1b96
sagaruppuluri <33374179+sagaruppuluri@users.noreply.github.com>
2019-03-23 16:57:14 +0530

    Update QuickloanApplication.java
    Added Test comment

--- src/main/java/com/initiative/quickloan/QuickloanApplication.java
+++ src/main/java/com/initiative/quickloan/QuickloanApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+// Test Comment 1 
 @SpringBootApplication
 @ComponentScan("com.initiative.quickloan.config")
 public class QuickloanApplication {
