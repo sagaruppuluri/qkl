f662a8c6834900e1f558ac2c2695cbfd69a989da
sagaruppuluri <33374179+sagaruppuluri@users.noreply.github.com>
2019-03-25 21:25:29 +0530

    Update QuickloanConfig.java
    Config comment added

--- src/main/java/com/initiative/quickloan/config/QuickloanConfig.java
+++ src/main/java/com/initiative/quickloan/config/QuickloanConfig.java
@@ -3,6 +3,7 @@ package com.initiative.quickloan.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+// Config comment 1 
 @Configuration
 @ComponentScan("com.initiative.quickloan.api")
 public class QuickloanConfig {
